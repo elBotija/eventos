1a3dc259df2546a2febaf495e3a7ded200c3a4ef
mmartinez <mmartinez@dosalcubo.com>
2019-01-18 00:20:30 -0300

    Updates

---
+++ precache-manifest.150f519bf945bec8ff57300b183edc29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40eae8546012d9151b77",
+    "url": "/eventos/static/js/runtime~main.40eae854.js"
+  },
+  {
+    "revision": "8e262a3fb7f7cf225d8b",
+    "url": "/eventos/static/js/main.8e262a3f.chunk.js"
+  },
+  {
+    "revision": "fea9c8b34d6e3b807e18",
+    "url": "/eventos/static/js/1.fea9c8b3.chunk.js"
+  },
+  {
+    "revision": "8e262a3fb7f7cf225d8b",
+    "url": "/eventos/static/css/main.0a88a8c0.chunk.css"
+  },
+  {
+    "revision": "f7fd1b0ac449042b5e66ebb4489a8250",
+    "url": "/eventos/index.html"
+  }
+];
